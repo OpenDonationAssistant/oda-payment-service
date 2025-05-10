fb58229748b08406d271671d3ed93a6859004927
stCarolas <stcarolas@gmail.com>
2025-05-10 23:57:26 +0300

    add missing files

---
+++ src/main/java/io/github/opendonationassistant/gateway/command/DeleteGateway.java
@@ -0,0 +1,42 @@
+package io.github.opendonationassistant.gateway.command;
+
+import io.github.opendonationassistant.commons.micronaut.BaseController;
+import io.github.opendonationassistant.gateway.repository.GatewayCredentialsDataRepository;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.authentication.Authentication;
+import io.micronaut.security.rules.SecurityRule;
+import io.micronaut.serde.annotation.Serdeable;
+import jakarta.inject.Inject;
+import java.util.Optional;
+
+@Controller
+public class DeleteGateway extends BaseController {
+
+  private GatewayCredentialsDataRepository repository;
+
+  @Inject
+  public DeleteGateway(GatewayCredentialsDataRepository repository) {
+    this.repository = repository;
+  }
+
+  @Post("/payments/commands/deletegateway")
+  @Secured(SecurityRule.IS_AUTHENTICATED)
+  public HttpResponse<Void> delete(
+    Authentication auth,
+    @Body DeleteGatewayCommand command
+  ) {
+    final Optional<String> ownerId = getOwnerId(auth);
+    if (ownerId.isEmpty()) {
+      return HttpResponse.unauthorized();
+    }
+    repository.deleteById(command.id());
+    return HttpResponse.ok();
+  }
+
+  @Serdeable
+  public static record DeleteGatewayCommand(String id) {}
+}
---
+++ src/test/java/io/github/opendonationassistant/gateway/command/DeleteGatewayTest.java
@@ -0,0 +1,60 @@
+package io.github.opendonationassistant.gateway.command;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import io.github.opendonationassistant.gateway.command.DeleteGateway.DeleteGatewayCommand;
+import io.github.opendonationassistant.gateway.repository.GatewayCredentialsData;
+import io.github.opendonationassistant.gateway.repository.GatewayCredentialsDataRepository;
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.security.authentication.Authentication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import java.util.Map;
+import java.util.Optional;
+import org.instancio.junit.Given;
+import org.instancio.junit.InstancioExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@MicronautTest(environments = "allinone")
+@ExtendWith(InstancioExtension.class)
+public class DeleteGatewayTest {
+
+  @Inject
+  SetGateway setGateway;
+
+  @Inject
+  DeleteGateway deleteGateway;
+
+  @Inject
+  GatewayCredentialsDataRepository repository;
+
+  @Test
+  public void testDeleteGateway(@Given String recipientId) {
+    var auth = mock(Authentication.class);
+    when(auth.getAttributes()).thenReturn(
+      Map.of("preferred_username", recipientId)
+    );
+
+    var createGatewayCommand = new SetGateway.SetGatewayCommand(
+      "tempId",
+      "gatewayId",
+      "token",
+      "gateway",
+      "secret",
+      "fiat",
+      false
+    );
+    setGateway.setGateway(auth, createGatewayCommand);
+
+    @NonNull
+    final Optional<GatewayCredentialsData> shouldExist = repository.findById(
+      "tempId"
+    );
+    assertTrue(shouldExist.isPresent());
+
+    deleteGateway.delete(auth, new DeleteGatewayCommand("tempId"));
+    assertTrue(repository.findById("tempId").isEmpty());
+  }
+}
