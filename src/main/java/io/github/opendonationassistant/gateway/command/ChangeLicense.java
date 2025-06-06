cc59e97d33dc67e2c48b4cf30258dbd4723468e9
stCarolas <stcarolas@gmail.com>
2025-06-07 00:27:11 +0300

    fix compilation

--- src/main/java/io/github/opendonationassistant/gateway/command/ChangeLicense.java
+++ src/main/java/io/github/opendonationassistant/gateway/command/ChangeLicense.java
@@ -8,6 +8,8 @@ import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.annotation.ExecuteOn;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.authentication.Authentication;
 import io.micronaut.security.rules.SecurityRule;
